93f9051b8d07c2668bbfdff6379d059e64ebfa8d
yk6 <grantex5@gmail.com>
2018-10-20 01:58:32 +0800

    Done 1.1 wait for bug appear

--- 4211.py
+++ 4211.py
@@ -8,23 +8,25 @@ pd.options.mode.chained_assignment = None
 
 #1.1
 
-# df = pd.read_csv("./4211 proj/dataport-export_gas_oct2015-mar2016.csv")
+df = pd.read_csv("./4211 proj/dataport-export_gas_oct2015-mar2016.csv")
 # data_id = df['dataid'].nunique()
 # print("number of house =", data_id)
 
-df = pd.read_csv("./short100data.csv")
+# df = pd.read_csv("./short10000data.csv")
 # df = pd.read_csv('./200k.csv')
 
 # Sort data into order by id, if same id then by time
-# Remove id with same meter reading as they are spoiled
 df.sort_values(["dataid", "localminute"], ascending=[True, True], inplace = True)
-# df.drop_duplicates(subset = ['dataid', 'meter_value'], keep = 'first', inplace = True)
 df.reset_index(drop = True, inplace = True)
 
+df.localminute = df.localminute.str.slice(0,19)
+df.localminute = pd.to_datetime(df.localminute, infer_datetime_format = True, format = "%Y/%m/%d %I:%M:%S %p")
 
 # two type of malfunction
 # 1: data reports back when change in gas use is < 2 cubic foot
 # 2: data reports back new meter_value is smaller than old meter_value
+# However, continuous report of same reading from same id over 12hrs,
+# the first reading after 12hrs will be treated as good reading
 
 # Progress bar cuz I always feel it is not working
 # f = FloatProgress(min=0, max=(len(df['dataid']) - 1))
@@ -33,17 +35,18 @@ df.reset_index(drop = True, inplace = True)
 # Create empty df and dictionary
 malfunction = pd.DataFrame(columns = ["localminute", "dataid", "meter_value"])
 bad_array = []
+to_drop = []
 
 prev_good = True
 _id = None
-prev_time = None
 _12hr = None
 # Sort malfunction with time period label
-# Append malfunction period to array
+# Assumption: the first data point is always correct as the 2nd pt is wrt to it, 3rd wrt to 2nd....etc
+# Find malfunction of type 1 and 2
+# Append malfunction period and data to respective array
 for row in df.itertuples():
     if(_id is None or _id != row.dataid):
         prev_good = True
-        prev_time = row.localminute
         _id = row.dataid
     # for estimating time take to process actual data
     if (row.Index == 0):
@@ -55,59 +58,27 @@ for row in df.itertuples():
             and ((row.meter_value - df.meter_value[row.Index-1]) < 2))):
         if(prev_good == True):
             bad_array.append([_id, row.localminute, row.localminute])
+            to_drop.append(row.Index)
+            # update 12hr pt
+            _12hr = row.localminute
         else:
-            bad_array[-1][2] = row.localminute
+            if ((row.localminute - _12hr) >= pd.to_timedelta("12:00:00")):
+                prev_good = True
+                continue
+            else:
+                bad_array[-1][2] = row.localminute
+                to_drop.append(row.Index)
         prev_good = False
-        prev_time = row.localminute
     else:
-        prev_good = True
-        prev_time = row.localminute            
-#     f.value+=1
-# insert before array append after if(prev_good == True)
-# #############################################################
-# if (row.localminute - _12hr >= 12):
-#     bad_array.append([_id, row.localminute, row.localminute])
-#     prev_good = True
-#     prev_time = row.localminute
-# _12hr = row.localminute
-# #############################################################
-
-
-print("Bad array: %ds" %(timeit.default_timer() - start_time))
-
+        prev_good = True 
 
 #Remove some values to speed up the time, those removed time are kept tracked in bad_array
-df.drop_duplicates(subset = ['dataid', 'meter_value'], keep = 'first', inplace = True)
-df.reset_index(drop = True, inplace = True)
-
-temp_index_holder = []
-
-# Assumption: the first data point is always correct as the 2nd pt is wrt to it, 3rd wrt to 2nd....etc
-# Find malfunction of type 1 and 2
-#######################################
-# malfunction.loc[row.Index] = row[1:4] 
-#######################################
-for row in df.itertuples():
-    # for estimating time take to process actual data
-    if (row.Index == 0):
-        continue
-    if (((row.meter_value <= df.meter_value[row.Index-1]) 
-         and (row.dataid == df.dataid[row.Index-1])) 
-        or ((row.localminute != df.localminute[row.Index-1]) 
-            and (row.dataid == df.dataid[row.Index-1]) 
-            and ((row.meter_value - df.meter_value[row.Index-1]) < 2))):
-        temp_index_holder.append(row.Index)    
-    
-print("Copy to malfunction: %ds" %(timeit.default_timer() - start_time))
-
-df.drop(index = temp_index_holder, inplace = True)
+df.drop(index = to_drop, inplace = True)
 df.reset_index(drop = True, inplace = True)
 
 # # Cast to int64
 # malfunction['dataid'] = malfunction['dataid'].astype(np.int64)
 # malfunction['meter_value'] = malfunction['meter_value'].astype(np.int64)
-
-# malfunction.reset_index(drop = True, inplace = True)
 
 # # Merge 2 df to compare diff, if one of the label value is different,
 # # value in _merge label will be different hence being able to differeniate the difference
