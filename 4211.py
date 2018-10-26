4b805e2f1aeaea91961f759fb09d0d0d460ec78d
yk6 <grantex5@gmail.com>
2018-10-27 03:14:02 +0800

    Add in 1.1 ipynb and update .py

--- 4211.py
+++ 4211.py
@@ -4,37 +4,47 @@ from ipywidgets import FloatProgress
 from IPython.display import display
 import timeit
 from datetime import timedelta
-start_time = timeit.default_timer() * 1000
-# pd.options.mode.chained_assignment = None 
+from pathlib import Path
+# start_time = timeit.default_timer() * 1000
+currentPath = str(Path().resolve())
+# Put raw csv in same 
+path = currentPath + "./csv/dataport-export_gas_oct2015-mar2016.csv"
 
-#1.1
+# Read in raw data and find 
+df = pd.read_csv(path)
+data_id = df['dataid'].nunique()
+print("Number of houses =", data_id)
 
-# df = pd.read_csv("./4211 proj/csv/dataport-export_gas_oct2015-mar2016.csv")
-# data_id = df['dataid'].nunique()
-# print("number of house =", data_id)
-
-df = pd.read_csv("./4211 proj/csv/sorted100.csv")
-# df = pd.read_csv('./200k.csv')
+# Read in raw data and find 
+df = pd.read_csv(path)
+data_id = df['dataid'].nunique()
+print("Number of houses =", data_id)
 
 # Sort data into order by id, if same id then by time
 df.sort_values(["dataid", "localminute"], ascending=[True, True], inplace = True)
 df.reset_index(drop = True, inplace = True)
 
+# Cast localminute into proper time format for further processing
 df.localminute = df.localminute.str.slice(0,19)
 df.localminute = pd.to_datetime(df.localminute, infer_datetime_format = True, format = "%Y/%m/%d %I:%M:%S %p")
+print(df.localminute[0])
+print("Length of df:", len(df))
 
-# two type of malfunction
+# Types of malfunction
 # 1: data reported back when change in gas use is < 2 cubic foot
 # 2: data reported back new meter_value is smaller than old meter_value
-# 3: data reported back is >2, but time > 15s, threshold value = 7/hr (US household average daily usage = 168)
+# 3: data reported back is >2, but time > 15s, threshold value for reporting malfunction will be 7 cubic foot/hr 
+# (US household average daily usage = 168 cubic foot)
 # 4: However, continuous report of same reading from same id over 12hrs,
-# the first reading after 12hrs will be treated as good reading
+# the first reading after 12hrs will be treated as good reading,
+# as the gas company will probably want to know whether is a meter malfunctioning
+# even though its reading did not change for a period of time
 
 # Progress bar cuz I always feel it is not working
 # f = FloatProgress(min=0, max=(len(df['dataid']) - 1))
 # display(f)
 
-# Create empty df and dictionary
+# Create empty df and array for more efficient removal of item in df
 malfunction = pd.DataFrame(columns = ["localminute", "dataid", "meter_value"])
 bad_array = []
 to_drop = []
@@ -44,7 +54,6 @@ _id = None
 _12hr = None
 # Sort malfunction with time period label
 # Assumption: the first data point is always correct as the 2nd pt is wrt to it, 3rd wrt to 2nd....etc
-# Find malfunction of type 1 and 2
 # Append malfunction period and data to respective array
 for row in df.itertuples():
     if(_id is None or _id != row.dataid):
@@ -53,6 +62,7 @@ for row in df.itertuples():
     # for estimating time take to process actual data
     if (row.Index == 0):
         continue
+    #1 and #2
     if (((row.meter_value <= df.meter_value[row.Index-1])
          and (row.dataid == df.dataid[row.Index-1])) 
         or ((row.localminute != df.localminute[row.Index-1]) 
@@ -61,9 +71,10 @@ for row in df.itertuples():
         if(prev_good == True):
             bad_array.append([_id, row.localminute, row.localminute])
             to_drop.append(row.Index)
-            # update 12hr pt
+            #4, update 12hr pt
             _12hr = row.localminute
         else:
+            #4  
             if ((row.localminute - _12hr) >= pd.to_timedelta("12:00:00")):
                 prev_good = True
                 continue
@@ -72,13 +83,16 @@ for row in df.itertuples():
                 to_drop.append(row.Index)
         prev_good = False
     else:
-        prev_good = True  
+        prev_good = True
+        #3
         if(((row.meter_value - df.meter_value[row.Index-1]) > 2) 
            and ((((row.localminute - df.localminute[row.Index-1]) / timedelta(hours = 1)) * 7) 
                 > (row.meter_value - df.meter_value[row.Index - 1]))):
             bad_array[-1][2] = row.localminute
+print("Length of bad_array:", len(bad_array))
+print("Length of to_drop:", len(to_drop))
 
-#Remove some values to speed up the time, those removed time are kept tracked in bad_array
+#Remove malfunction data to produce clean df
 df.drop(index = to_drop, inplace = True)
 df.reset_index(drop = True, inplace = True)
 
@@ -93,18 +107,20 @@ df.reset_index(drop = True, inplace = True)
 # .drop(['_merge'], axis=1)\
 # .reset_index(drop=True)
 
-# Make bad_array into df and transform it for shape to be correct
+# Convert bad_array into df and transform it for shape to be correct
 malfunction = pd.DataFrame(bad_array, columns = ['dataid', 'start_time', 'end_time'])
 malfunction.T
 
+# Save part1 result into csv for easier access in part2
 df.to_csv('./clean_df.csv', index = False)
 malfunction.to_csv('./malfunction.csv', index = False)
 
 # pd.set_option('display.max_rows', 10000)
 
-elapsed = timeit.default_timer() * 1000 - start_time
-print("total: %ds" %(elapsed/1000)) if ((elapsed > 5000) == True) else print("total: %dms" %elapsed)
-#1.2
+# elapsed = timeit.default_timer() * 1000 - start_time
+# print("total: %ds" %(elapsed/1000)) if ((elapsed > 5000) == True) else print("total: %dms" %elapsed)
+print("length of df:", len(df))
+print("length of malfunction:", len(malfunction))
 
 import pandas as pd
 import numpy as np
